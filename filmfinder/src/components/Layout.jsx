d984f9829743aa5c6b79b8441a98943cd9307d86
Sean Maida <smaida2002@yahoo.com>
2026-06-19 17:36:49 -0400

    added search bar

--- filmfinder/src/components/Layout.jsx
+++ filmfinder/src/components/Layout.jsx
@@ -1,15 +1,27 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Box from '@mui/material/Box'
 import "../App.css";
 
-export default function Layout() {
+export default function Layout({children}) {
   return (
-    <Container maxwWidth="lg">
+    <Container>
       <Navbar className="navbar">
         <Container className="justify-content-center">
           <Navbar.Brand href="/">FilmFinder</Navbar.Brand>
         </Container>
       </Navbar>
+
+      <Box
+        component="main"
+        sx={{
+            flexGrow: 1,
+            bgcolor: '#ffffff',
+            minHeight: '100vh',
+        }}
+        >
+            {children}
+        </Box>
     </Container>
   );
 }
--- filmfinder/src/pages/Home.jsx
+++ filmfinder/src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import Layout from "../components/Layout";
+import TextField from '@mui/material/TextField';
 
 export default function Home() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <br />
+      <TextField fullWidth label="Search Movie Recommendations" id="movieSearch" />
+    </Layout>
+  );
 }
